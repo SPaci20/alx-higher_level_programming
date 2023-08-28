d35c93ef066660031e0729f1e74e2d03c2bcc5df
SPaci20 <spaci20@gmail.com>
2023-08-28 07:23:40 -0700

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -9,5 +9,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return(True)
-    except (TypeError, ValueError):
+    except(TypeError, ValueError):
         return(False)
