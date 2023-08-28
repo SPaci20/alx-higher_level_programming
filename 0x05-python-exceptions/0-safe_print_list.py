50e159bdfb457717e8ca95a204fb22b7dc040b84
SPaci20 <spaci20@gmail.com>
2023-08-28 06:57:37 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            print(my_list[i], end="")
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             count += 1
-        print()  # Print a newline after printing elements
-        return count
-    except IndexError:  # Handle the case when x is larger than the list length
-        print()  # Print a newline if the list is exhausted before x elements
-        return count
+        except IndexError:
+            break
+        print("")
+        return (count)
