2ff0f3a111f2f278971f086b846c34270fd8db94
SPaci20 <spaci20@gmail.com>
2023-08-29 11:17:36 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+
+
+"""Define class called Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initialize a new Square
+        args:
+            size : Size of the square
+        """
+        self.__size = size
