c7041f86c97f107c3e3ff74f99ed34169784edf2
SPaci20 <spaci20@gmail.com>
2023-09-04 06:28:54 -0700

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Define a Rectangle class."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a rectangle with optional width and height."""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter method for width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width attribute."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height attribute."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
