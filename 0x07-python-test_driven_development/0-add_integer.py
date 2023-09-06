8a2d75abb3e54c191d51d6f491b9ce433e5012cb
SPaci20 <spaci20@gmail.com>
2023-09-06 13:53:58 -0700

    Task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
+"""Function that adds 2 integers."""
+
 
 def add_integer(a, b=98):
     """
-    Adds two integers.
+    Add two integers.
 
-    :param a: The first integer.
-    :param b: The second integer (default is 98).
-    :return: The addition of a and b as an integer.
+    Parameters:
+    a: int or float
+        First integer or float value.
+    b: int or float, optional (default=98)
+        Second integer or float value.
+
+    Returns:
+    int: The addition of 'a' and 'b' as integers.
+
+    Raises:
+    TypeError: If 'a' or 'b' is not an integer or float with specific error messages.
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-    # Cast a and b to integers if they are floats
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+
     a = int(a)
     b = int(b)
 
