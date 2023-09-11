4e9e5a80d9ccb4fac6cdeb2b8c0645198c7f67bb
SPaci20 <spaci20@gmail.com>
2023-09-11 10:30:35 -0700

    Task 0 Upd

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
 def lookup(obj):
-    """Return a list of available attributes and methods of an object."""
+    """
+    Return a list of available attributes and methods of an object.
+
+    :param obj: The object to inspect.
+    :return: A list of attribute and method names.
+    """
     return dir(obj)
