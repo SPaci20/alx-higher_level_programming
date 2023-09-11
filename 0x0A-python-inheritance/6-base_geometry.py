733a0a957ea51eac7c1c3661f4dcc90b869809c3
SPaci20 <spaci20@gmail.com>
2023-09-11 14:44:48 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent BaseGeometry class."""
+    def area(self):
+        """Not Implemented."""
+        raise Exception("area() is not implemented")
