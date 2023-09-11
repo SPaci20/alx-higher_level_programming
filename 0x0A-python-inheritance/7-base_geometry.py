a69c8ba9781e0a384d106a5031ccd6783ad04ad1
SPaci20 <spaci20@gmail.com>
2023-09-11 15:14:23 -0700

    Task 9

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate Value."""
+        """Validate an integer."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent BaseGeometry."""
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate an integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+"""Define a class Rectangle, inherited from BaseGeometry."""
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle class."""
+    def __init__(self, width, height):
+        """Instatiation of width and height."""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        """Return area."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str()."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
