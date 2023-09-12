de04845acad4dc408a519ae0940ccece6750104f
SPaci20 <spaci20@gmail.com>
2023-09-12 16:04:48 -0700

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that appends a string at the end of a text file."""
+
+
+def append_write(filename="", text=""):
+    """Use with and returns the number of characters added."""
+    try:
+        with open(filename, 'a', encoding='utf-8') as file:
+            num_chars_added = file.write(text)
+        return num_chars_added
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
