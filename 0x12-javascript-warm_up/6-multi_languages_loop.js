c940677d96ceef7a5c06c3ab32d613e14fefb7fa
SPaci20 <spaci20@gmail.com>
2023-11-13 22:56:16 +0200

    Task6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints 3 lines but by using an array of string and a loop
+
+let i = 0;
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
+}
