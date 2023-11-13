29ef125878f8c38a1057b9d356b38ebc3a3d7d69
SPaci20 <spaci20@gmail.com>
2023-11-13 23:11:55 +0200

    Task10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// computes and prints a factorial
+
+function factorial (a) {
+  if (a > 0) {
+    return a * factorial(a - 1);
+  } else {
+    return 1;
+  }
+}
+
+const a = parseInt(process.argv[2]);
+const result = factorial(a);
+console.log(result);
