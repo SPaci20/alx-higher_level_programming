8eaea26f5723890d2308d4b4c94d16f531d8188a
SPaci20 <spaci20@gmail.com>
2023-11-14 21:17:03 +0200

    Adv Tasks upd

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,21 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Check if the correct number of command-line arguments is provided
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-
-const [, , sourceFileA, sourceFileB, destinationFile] = process.argv;
-
-// Read the contents of the source files
-const contentA = fs.readFileSync(sourceFileA, 'utf8');
-const contentB = fs.readFileSync(sourceFileB, 'utf8');
-
-// Concatenate the contents
-const concatenatedContent = `${contentA}${contentB}`;
-
-fs.writeFileSync(destinationFile, concatenatedContent);
-
-console.log(`Files ${sourceFileA} and ${sourceFileB} were successfully concatenated to ${destinationFile}`);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
