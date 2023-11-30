21602ae005786b776684f581d540851c45f8b90c
SPaci20 <spaci20@gmail.com>
2023-11-30 16:21:57 +0200

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-body_size=$(curl -s "$1" | wc -c)
-
-echo "$body_size"
+curl -s "$1" | wc -c
