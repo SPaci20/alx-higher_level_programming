0bc0195c2ea1a5583c6a57e1d3703ea6d0f354c8
SPaci20 <spaci20@gmail.com>
2023-12-01 04:36:51 -0800

    Task 6 upd1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-A script that:
+"""A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header of the response.
@@ -13,13 +12,11 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     try:
-        # Create a request object
         request = urllib.request.Request(url)
-
-        # Send the request and get the response
         with urllib.request.urlopen(request) as response:
-            # Access the X-Request-Id header using get to avoid exceptions
             x_request_id = dict(response.headers).get("X-Request-Id")
             print(x_request_id)
-    except (URLError, HTTPError) as e:
-        print(f"Error: {e}")
+    except URLError as e:
+        print(f"Error: {e.reason}")
+    except HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
