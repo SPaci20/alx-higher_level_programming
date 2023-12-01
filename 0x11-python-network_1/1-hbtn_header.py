8890eb6a578798a9c823848baebc8091d5df013a
SPaci20 <spaci20@gmail.com>
2023-12-01 04:04:44 -0800

    Task 1 upd

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
 """
-This script takes in a URL, sends a request, and displays the value of the
-X-Request-Id variable found in the header of the response.
+Script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header of the response.
 """
-
-import urllib.request
 import sys
+import urllib.request
+from urllib.error import URLError, HTTPError
 
-
-def fetch_x_request_id(url):
-    """
-    Fetches the X-Request-Id value from the header of the response.
-
-    Args:
-        url (str): The URL to fetch.
-
-    Returns:
-        str: The X-Request-Id value or a message if the header is not found.
-    """
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        if x_request_id:
-            return x_request_id
-        else:
-            return "X-Request-Id header not found in the response."
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
+        print("Usage: {} <URL>".format(sys.argv[0]))
         sys.exit(1)
 
     url = sys.argv[1]
-    result = fetch_x_request_id(url)
-    print(result)
+
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            x_request_id = dict(response.headers).get("X-Request-Id")
+            print(x_request_id)
+    except (URLError, HTTPError) as e:
+        print(f"Error: {e}")
