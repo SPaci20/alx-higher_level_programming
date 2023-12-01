83fca6903d26eb488c902dc22cf963f4522d81a1
SPaci20 <spaci20@gmail.com>
2023-12-01 03:35:49 -0800

    Task 0 Upd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script fetches the status of https://alx-intranet.hbtn.io.
+Script fetches the status of https://alx-intranet.hbtn.io
 """
 
 import urllib.request
@@ -8,7 +8,7 @@ import urllib.request
 
 def fetch_status(url):
     """
-    Fetches the status of the given URL and displays response
+    Fetches the status of the given URL and displays response.
 
     Args:
         url (str): The URL to fetch.
@@ -25,3 +25,8 @@ def fetch_status(url):
         print("    - type:", type(content))
         print("    - content:", content)
         print("    - utf8 content:", content.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_status(url)
