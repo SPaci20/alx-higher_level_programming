67a5e211cdab87d66a12a2ba7e0f140bcfcb4f59
SPaci20 <spaci20@gmail.com>
2023-12-01 04:47:09 -0800

    Task 7 upd

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,17 +4,15 @@
 - sends a request to the URL
 - displays the body of the response.
 """
-
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raises HTTPError for bad responses
-
-        print(response.text)
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
