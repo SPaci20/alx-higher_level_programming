de401edc4c3ba07cf2646995dcce69d8aa5b4bfa
SPaci20 <spaci20@gmail.com>
2023-12-01 04:23:04 -0800

    Task 4 upd

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,11 +6,21 @@ using package requests.
 
 import requests
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
 
-    # Display information about the response
-    print("Body response:")
-    print(f"    - type: {type(response.text)}")
-    print(f"    - content: {response.text}")
+def fetch_status():
+    """
+    Sends a GET request to https://intranet.hbtn.io/status
+    and prints information about the response.
+    """
+    try:
+        response = requests.get("https://intranet.hbtn.io/status")
+        response.raise_for_status()  # Raises HTTPError for bad responses
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    fetch_status()
