2d6cf52391d5d28e6ee74c44b1524e14a06e3f0d
SPaci20 <spaci20@gmail.com>
2023-12-01 04:45:35 -0800

    Task 7 upd

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Script that:
-- takes in a URL, sends a request to the URL, displays response
--HTTP status code is greater than or equal to 400,
-print: Error code: followed by the value of the HTTP status code
--You must use the packages requests and sys
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
 
 import sys
@@ -13,11 +11,10 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raises HTTPError for bad responses
 
-    # Display the body of the response
-    print(response.text)
-
-    # Check if the HTTP status code is greater than or equal to 400
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
+        print(response.text)
+    except requests.RequestException as e:
+        print(f"Error: {e}")
