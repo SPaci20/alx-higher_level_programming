16189d29f58ab548282424baab91b378a2378967
SPaci20 <spaci20@gmail.com>
2023-12-01 04:38:47 -0800

    Task 6 upd2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,19 +4,19 @@
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header of the response.
 """
+
 import sys
-import urllib.request
-from urllib.error import URLError, HTTPError
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
 
-    try:
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            x_request_id = dict(response.headers).get("X-Request-Id")
-            print(x_request_id)
-    except URLError as e:
-        print(f"Error: {e.reason}")
-    except HTTPError as e:
-        print(f"HTTP Error: {e.code} - {e.reason}")
+    # Define the payload with the email parameter
+    payload = {'email': email}
+
+    # Send a POST request
+    response = requests.post(url, data=payload)
+
+    # Display the body of the response
+    print("Your email is:", response.text)
