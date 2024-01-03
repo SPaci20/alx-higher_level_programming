bf9032edb34b3b712b6170d3295f3c754fa901b1
SPaci20 <spaci20@gmail.com>
2024-01-03 05:50:09 -0800

    minor  update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles' character ID
-
-request(apiUrl, (error, response, body) => {
-  try {
-    if (error) {
-      throw new Error(`Request failed: ${error.message}`);
-    }
-
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body).results;
-      const count = filmsData.reduce((acc, film) => {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-          return acc + 1;
-        }
-        return acc;
-      }, 0);
-
-      console.log(count);
-    } else {
-      throw new Error(`Failed to retrieve films data. Status code: ${response.statusCode}`);
-    }
-  } catch (parseError) {
-    console.error(`Error parsing JSON: ${parseError.message}`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
