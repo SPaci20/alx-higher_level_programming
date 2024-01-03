e63def3ee4ca2d16828a70c938db3d613cbdfc83
SPaci20 <spaci20@gmail.com>
2024-01-03 05:59:22 -0800

    How many completed?

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,24 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  try {
-    if (error) {
-      throw new Error(`Request failed: ${error.message}`);
-    }
-
-    if (response.statusCode === 200) {
-      fs.writeFileSync(filePath, body, 'utf-8');
-      console.log(`Body content successfully written to ${filePath}`);
-    } else {
-      throw new Error(`Failed to retrieve webpage data. Status code: ${response.statusCode}`);
-    }
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
